c314305288b9b49889cdd9ed61675a2df326e366
luis <lagonzalez1688@gmail.com>
2020-09-30 00:18:37 -0700

    Ejercicio "hola mundo"

---
+++ src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio1;
+
+
+public class Ejercicio1 {
+
+    
+    public static void main(String[] args) {
+        System.out.print("HOLA MUNDO ");
+    }
+    
+}
